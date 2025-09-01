c61de234dae42f660949de8b0b20007d546676ec
gollapallijayanthi <148422162+gollapallijayanthi@users.noreply.github.com>
2025-09-01 23:04:39 +0530

    Time: 2 ms (100%), Space: 41.1 MB (100%) - LeetHub

--- 3994-find-the-least-frequent-digit/3994-find-the-least-frequent-digit.java
+++ 3994-find-the-least-frequent-digit/3994-find-the-least-frequent-digit.java
@@ -4,19 +4,19 @@ class Solution {
         int temp=n;
         while(temp>0)
         {
-            int r=temp%10;
-            temp=temp/10;
+            int r=temp%10; 
+            temp=temp/10; 
             mp.put(r,mp.getOrDefault(r,0)+1);
         }
         int leastnum=Integer.MAX_VALUE,leastfreq=Integer.MAX_VALUE;
         for(Map.Entry<Integer,Integer>entry:mp.entrySet())
         {
-            int key=entry.getKey();
-            int val=entry.getValue();
-            if(val<leastfreq)
+            int key=entry.getKey(); 
+            int val=entry.getValue(); 
+            if(val<leastfreq) 
             {
-                 leastfreq=val;
-                    leastnum=key;
+                 leastfreq=val; 
+                    leastnum=key; 
                 if(leastfreq==val)
                 {
                     if(key>leastnum)
