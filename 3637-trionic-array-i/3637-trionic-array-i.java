3bfa5a447d09233b0b85779ed8ed33422f5f1471
gollapallijayanthi <148422162+gollapallijayanthi@users.noreply.github.com>
2026-02-03 20:13:25 +0530

    Time: 1 ms (72.27%), Space: 44.8 MB (31.09%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        if (n < 3) return false;
+
+        for (int p = 1; p < n - 2; p++) {
+            if (!isStrictlyIncreasing(nums, 0, p)) continue;
+
+            for (int q = p + 1; q < n - 1; q++) {
+                if (isStrictlyDecreasing(nums, p, q) && isStrictlyIncreasing(nums, q, n - 1)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isStrictlyIncreasing(int[] nums, int start, int end) {
+        for (int i = start; i < end; i++) {
+            if (nums[i] >= nums[i + 1]) return false;
+        }
+        return true;
+    }
+
+    private boolean isStrictlyDecreasing(int[] nums, int start, int end) {
+        for (int i = start; i < end; i++) {
+            if (nums[i] <= nums[i + 1]) return false;
+        }
+        return true;
+    }
+}
