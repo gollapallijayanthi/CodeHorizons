0d556a5544822c4e20a8c79796b3b3b73b6d39f3
gollapallijayanthi <148422162+gollapallijayanthi@users.noreply.github.com>
2026-05-30 14:22:38 +0530

    Time: 39 ms (1.36%), Space: 19.4 MB (67.14%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,17 +1,15 @@
 class Solution:
-    def sol(self,nums,res,curr,ind):
-        if ind==len(nums):
-            res.append(list(curr))
-            return 
-        
-        curr.append(nums[ind])
-        self.sol(nums,res,curr,ind+1)
-
-        curr.pop()
-        self.sol(nums,res,curr,ind+1)
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res=[]
-        curr=[]
-        self.sol(nums,res,curr,0)
+        for i in range(pow(2,len(nums))):
+            curr=[]
+            num=1
+            idx=0
+            while idx<len(nums):
+                if num&i>0:
+                    curr.append(nums[idx])
+                num=num<<1
+                idx=idx+1
+            res.append(list(curr))
         return res
         
